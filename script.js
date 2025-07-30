17ddca09f8c32bb176f98438ca741d3ed9fce408
mynewdomain <newgitacc@gmail.com>
2025-07-30 14:19:21 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,6 @@
 var container=document.getElementById("container");
-/*var row=document.creatElement("div");
-row.style.display="flex";
-row.style.flexDirection="row";
-row.style.justifyContent="space-around";
-row.style.wrap="wrap";
-var rowChildren=row.children;
-var screenWidth=window.innerWidth;
-if(rowChildren.length<screenwidth){
-
-}*/
 var pageNames=["Weather App","Notes App",];
-var pageURLS=["./weatherApp","./notesApp"]
+var pageURLS=["../weatherApp","../notesApp"]
 for(var i=0;i<pageNames.length;i++){
     var page=document.createElement("div");
     page.className="pages";
