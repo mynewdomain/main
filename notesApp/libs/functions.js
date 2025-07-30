873a0ad506832ba2674056b387bc58d5d754fb59
mynewdomain <newgitacc@gmail.com>
2025-07-30 22:54:28 +0300

    Update functions.js

--- notesApp/libs/functions.js
+++ notesApp/libs/functions.js
@@ -7,3 +7,16 @@ function editMode(id){
 function noteId(){
     return Date.now().toString();
 }
+function openDialog(bg){
+    bg.style.display="flex";
+}
+function closeDialog(bg) {
+    bg.style.display = "none";
+    displayNote();
+}
+function confirmReset() {
+  if (confirm("Θες σίγουρα να διαγράψεις ΟΛΕΣ τις σημειώσεις;")) {
+    localStorage.clear();
+    location.reload();
+  }
+}
