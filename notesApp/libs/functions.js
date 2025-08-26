ce1fba59a335f9e04075b9352ce0f1dd9d692b83
mynewdomain <newgitacc@gmail.com>
2025-08-26 14:41:00 +0300

    Update functions.js

--- notesApp/libs/functions.js
+++ notesApp/libs/functions.js
@@ -4,6 +4,26 @@ function editMode(id){
     }
     return false;
 }
+function noteId(){
+    return Date.now().toString();
+}
+function createdAt(){
+    var d=new Date();
+    var day=d.getDate();
+    var month=d.getMonth()+1;
+    var year=d.getFullYear();
+    var hours=d.getHours();
+    var minutes=d.getMinutes();
+    if(hours<10){
+        hours="0"+hours;
+    }
+    if(minutes<10){
+        minutes="0"+minutes;
+    }
+    var time=hours+":"+minutes;
+    var date=day+"/"+month+"/"+year+" "+time;
+    return date;
+}
 function openDialog(bg){
     bg.style.display="flex";
 }
@@ -18,39 +38,45 @@ function confirmReset() {
   }
 }
 function theme(opt){
+    var theme;
     var bg;
-    var color;
+    var mainColor;
+    var title;
     if(opt=="light"){
-        bg="white";
-        color="black";
+        bg="rgb(240, 240, 240)";
+        mainColor="black";
+        title="black";
+        theme=opt;
     }else if(opt=="dark"){
         bg="black";
-        color="white";
+        mainColor="black";
+        theme=opt;
     }else{
         bg="";
-        color="";
+        mainColor="";
+        theme="";
     }
-    localStorage.setItem("theme",bg);
-    localStorage.setItem("color",color);
-    var back=localStorage.getItem("theme");
+    var theme=localStorage.getItem("theme");
+    var back=localStorage.getItem("themeBg");
     var color=localStorage.getItem("color");
+    localStorage.setItem("themeBg",bg);
+    localStorage.setItem("color",mainColor);
+    localStorage.setItem("theme",theme);
     console.log(back);
     container.style.backgroundColor=back;
     container.style.color=color;
-    
+    document.getElementById("themeSelect").value=theme;
 }
-function createdAt(){
-    var d=new Date();
-    var day=d.getDate();
-    var month=d.getMonth()+1;
-    var year=d.getFullYear();
-    var hours=d.getHours();
-    var minutes=d.getMinutes();
-    if(hours<10 || minutes<10){
-        hours="0"+hours;
-        minutes="0"+minutes;
-    }
-    var time=hours+":"+minutes;
-    var date=day+"/"+month+"/"+year+" "+time;
-    return date;
+function toDate(dateString) {
+    var dateObj = dateString.split(" ");
+    var date = dateObj[0];
+    var time = dateObj[1];
+    var splittedDate = date.split("/");
+    var splittedTime = time.split(":");
+    var day = parseInt(splittedDate[0], 10);
+    var month = parseInt(splittedDate[1], 10) - 1; // JS months are 0-based
+    var year = parseInt(splittedDate[2], 10);
+    var hours = parseInt(splittedTime[0], 10);
+    var minutes = parseInt(splittedTime[1], 10);
+    return new Date(year, month, day, hours, minutes);
 }
