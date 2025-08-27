2f0ed9082dfc4815bb5cc02ca076282ef04f0bff
mynewdomain <newgitacc@gmail.com>
2025-08-27 22:40:24 +0300

    Update main.py

--- python/main.py
+++ python/main.py
@@ -1,1 +1,23 @@
-
+import pyScrape
+import requests
+import os
+DISCORD_WEBHOOK = os.getenv("DISCORD_WEBHOOK")
+ANNOUNCEMENTS_URL = os.getenv("ANNOUNCEMENTS_URL")
+#url="https://www.uniwa.gr/category/announcements/"
+file='anakoinwseis.txt'
+def send_message(message):
+    if DISCORD_WEBHOOK:
+        requests.post(DISCORD_WEBHOOK, json={
+            "content": f"{message}\n{ANNOUNCEMENTS_URL}"
+        })
+stored_dates = pyScrape.load_data(file)
+storedData = len(stored_dates)
+dates=pyScrape.getAnnouncements(ANNOUNCEMENTS_URL)
+newData=len(dates)
+if newData==storedData:
+    send_message("Δεν βγήκε ανακοίνωση")
+else:
+    diaf=newData-storedData
+    print("There are "+str(diaf)+" new announcements")
+    pyScrape.save_data(file, dates)
+    send_message("Βγήκαν "+str(diaf)+" ανακοινώσεις")
