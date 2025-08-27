743d19c9a5033d3ddc78736625f9a27560977c66
mynewdomain <newgitacc@gmail.com>
2025-08-27 23:47:48 +0300

    Update pyScrape.py

--- python/pyScrape.py
+++ python/pyScrape.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
-f=open(path,"w")
-f.close()
+import os
+
 def getAnnouncements(site):
     url=site
     response=requests.get(url)
@@ -14,13 +14,18 @@ def getAnnouncements(site):
         year = i.find('span', class_='date_year').text.strip()
         dates.append(day+"/"+month+"/"+year)
     return dates
-def save_data(path,lista):
-    f=open(path,"w")
-    for i in lista:
-        f.write(str(i)+"\n")
-    f.close()
+
+def save_data(path, lista):
+    with open(path, "w") as f:
+        for i in lista:
+            f.write(str(i) + "\n")
+
 def load_data(path):
-    f=open(path,"r")
-    lines=f.read().splitlines()
-    f.close()
+    if not os.path.exists(path):
+        # Δημιουργούμε άδειο αρχείο αν δεν υπάρχει
+        with open(path, 'w') as f:
+            pass
+        return []
+    with open(path, "r") as f:
+        lines = f.read().splitlines()
     return lines
